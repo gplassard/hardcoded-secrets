1161b57358d472b389ad7414ad9a8e5e20944a21
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-01-03 20:15:36 +0100

    Create project

---
+++ src/test/java/fr/gplassard/hardcodedsecrets/HardcodedSecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gplassard.hardcodedsecrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardcodedSecretsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
